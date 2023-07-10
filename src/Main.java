3a1127227425f23b8226cdc689a3ff78d4d2a319
SAFOSKINSerghei <SAFOSKINSerghei@gmail.com>
2023-07-11 00:18:58 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import dotS.*;
 public class Main {
     public static void main(String[] args){
 
-        Complex c1;
+        Complex c1, c2;
         c1 = new Complex("1+3i2");
         System.out.print(c1.getAlgebraicForm(0));
 
