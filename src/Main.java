bfe4ed842b692a1aeebf19e26b51e36f57ca870b
Serioj <131291670+safoskinserghei@users.noreply.github.com>
2023-07-11 00:15:48 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Complex c1, c2, c3;
         c1 = new Complex("1+3i2");
         System.out.print(c1.getAlgebraicForm(0));
+        System.out.println("Finish!");
 
     }
 }
