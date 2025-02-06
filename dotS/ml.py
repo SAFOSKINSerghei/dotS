4044da07568b12f88d6db6981138ea13bd4986b0
serioj <SAFOSKINSerghei@gmail.com>
2025-02-06 16:59:58 +0200

    class for linear regression and multilinear regression

--- dotS/ml.py
+++ dotS/ml.py
@@ -1,18 +1,43 @@
 from sklearn.linear_model import LinearRegression
-import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
+from joblib import dump, load
+import numpy as np
+import pandas as pd
 
-datas = pd.read_csv("data.csv")
 
+class DotSLinearRegression:
 
-def safos_linear_regression(data):
-    x = pd.DataFrame(datas['x'])
-    y = pd.DataFrame(datas['y'])
-    x_tr, x_ts, y_tr, y_ts = train_test_split(x, y, test_size=0.2)
-    l_model = LinearRegression()
-    l_model.fit(x_tr, y_tr)
-    b = l_model.intercept_
-    scores = l_model.coef_
-    y_pr = l_model.predict(np.array([[1]]))
-    print(y_pr)
+    def __init__(self, x=None, y=None, test_size=0.1):
+        self.l_model = LinearRegression()
+        try:
+            self.test_size = test_size
+            self.x_tr, self.x_ts, self.y_tr, self.y_ts = train_test_split(x, y, test_size=test_size)
+            self.l_model.fit(self.x_tr, self.y_tr)
+        except:
+            pass
+
+    def change_dataset(self, x, y, test_size):
+        self.test_size = test_size
+        self.x_tr, self.x_ts, self.y_tr, self.y_ts = train_test_split(x, y, test_size=test_size)
+        self.l_model.fit(self.x_tr, self.y_tr)
+
+    def get_fitted_model(self):
+        return self.l_model
+
+    def get_b(self):
+        return self.l_model.intercept_[0]
+
+    def get_xs(self):
+        return pd.DataFrame(self.l_model.coef_)
+
+    def get_predict(self, x):
+        return self.l_model.predict([x])[0][0]
+
+    def save_model(self, file_name):
+        dump(self.l_model, file_name)
+
+    def load_model(self, file_name):
+        self.test_size = None
+        self.x_tr = self.x_ts = self.y_tr = self.y_ts = None
+        self.l_model = load(file_name)
+        return self.l_model
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import dotS.ml
+from  dotS.ml import *
+import pandas as pd
+import numpy as np
+
+
+datas = pd.read_csv("lr_datas.csv")
+x = pd.DataFrame(datas['x'])
+y = pd.DataFrame(datas['y'])
+lr_class = DotSLinearRegression(x, y, 0.2)
+print(lr_class.get_predict([1]))
+
+lr_class.load_model('test_model_2')
+print(lr_class.get_predict([1, 59, 117, 175]))
